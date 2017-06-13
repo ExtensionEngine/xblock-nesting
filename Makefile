651173139d9fe4b87c497e7b76d0229c9e69e2ae
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2017-06-13 17:02:47 +0000

    Returning css folder to repo

--- Makefile
+++
@@ -1,2 +0,0 @@
-compile-sass:
-	python scripts/compile_sass.py
--- scripts/compile_sass.py
+++
@@ -1,8 +0,0 @@
-import sass
-
-BASE_DIR = 'assessment_factory/public/'
-sass.compile(
-    dirname=(BASE_DIR+'sass', BASE_DIR+'css'),
-    include_paths=[BASE_DIR+'sass/vendor/bi-app'],
-    output_style='compressed',
-)
